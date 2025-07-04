b69831acfcae1d55c42232cb3088c40a4c891db2
Florian Mairhuber <florian.mairhuber@gmail.com>
2025-07-04 18:19:40 +0200

    Docker environment created

--- Test.py
+++
@@ -1,4 +0,0 @@
-# This is a simple Python script to print a message
-print("Today is a good day to learn Python and Neural Networks!")
-
-print("Test.py is running...")
---
+++ train.py
@@ -0,0 +1,7 @@
+# main file to run the neural network number prediction
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+
+
