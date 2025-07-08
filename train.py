9022efe0447f5615e3f268c9f0204d62e2da02df
Florian Mairhuber <florian.mairhuber@gmail.com>
2025-07-08 15:49:21 +0200

    first methods defined

--- train.py
+++ train.py
@@ -4,4 +4,31 @@ import torchvision
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 
+def load_data():
+    # load MNIST and DataLoader
+    pass
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # define layers 
+
+    def forward(self, x):
+        # forward pass
+        return x
+
+def train(model, loader, optimizer, loss_fn):
+    # train epoch
+    pass
+
+def test(model, loader, loss_fn):
+    # Accuracy and loss on test set
+    pass
+
+def main():
+    # connect everything
+    pass
+
+if __name__ == "__main__":
+    main()
 
